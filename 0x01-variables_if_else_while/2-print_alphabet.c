828b555f5a82e27ab82df76ba8ac1b4dbc0826c6
Dozie2001 <davidchidozie402@gmail.com>
2022-07-02 10:06:35 -0700

    Hey

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main entry - Prints the lower alphabet
+ * main entry - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
@@ -9,5 +11,6 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	return (0);
 }
