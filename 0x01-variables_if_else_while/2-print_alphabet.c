73563f8c860b29681ddf477cee66e985eb82158b
Dozie2001 <davidchidozie402@gmail.com>
2022-07-02 09:56:28 -0700

    Hey

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main entry - Prints the lower alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
