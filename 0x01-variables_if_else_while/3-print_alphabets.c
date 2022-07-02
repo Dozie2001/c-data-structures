6e1fc6aced0ee3cd44b766ddcb4f3afef8d04b59
Dozie2001 <davidchidozie402@gmail.com>
2022-07-02 10:40:53 -0700

    Hi

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print the alpahbet both small and big
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n';
+	return (0);
+}
