07f950c2fee7b827b7369e703249f3af691dce35
Dozie2001 <davidchidozie402@gmail.com>
2022-07-05 16:21:29 -0700

    Hey

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Gives absolute value
+ * @d: Input value
+ * Return:Returns abolute value
+ */
+int _abs(int d)
+{
+	if (d >= 0)
+		return (d);
+	else
+		return (d * -1);
+}
