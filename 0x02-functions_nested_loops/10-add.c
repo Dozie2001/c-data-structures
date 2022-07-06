18d196b960a1062b09809943897d188e3b09f8e4
Dozie2001 <davidchidozie402@gmail.com>
2022-07-06 03:20:01 -0700

    Hi

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Adds to values together
+ * @a: Input value
+ * @b: Input Value
+ * Return: Sum
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
