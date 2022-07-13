270af2d8e9b5d012e10b412ab3304b5d48ab70ef
Dozie2001 <davidchidozie402@gmail.com>
2022-07-12 17:05:54 -0700

    Hi

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,16 @@
  */
 void rev_string(char *s)
 {
-	int len = 0;
+	int len = 0, int i = 0;
+	char ax;
 
 	while (s[len] != '\0')
 		len++;
 
-	while (len)
+	while (i < len--)
 	{
-		--len;
-		s[len];
+		ax = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
 	}
 }
