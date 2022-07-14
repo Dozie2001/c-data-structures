74d64f604023e9f9b9aae56202c73ef6cfcad194
Dozie2001 <davidchidozie402@gmail.com>
2022-07-14 14:40:39 -0700

    5th project

---
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code 
+ * @n: an input array
+ * @a: number of elements of array
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337,};
+
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+	return (0);
+}
