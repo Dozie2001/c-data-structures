7b4c71f581efb8a6c6b2dec044c294121380c0dc
Dozie2001 <davidchidozie402@gmail.com>
2022-07-15 07:03:02 -0700

    1str project

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,22 @@
 #include "main.h"
 /**
- * _strcat - a function that concatenates two strings.
- * @dest: an input string
- * @src: an input string
- * Return: A pointer to the resulting string
+ * _strcat - A function that appends string int another
+ * Description: The function that is called off by a null byte
+ * @dest: An input pointer
+ * @src: An input pointer
+ *
+ * Return:pointer resulting to dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-	char *temp = dest;
+	char *point = dest;
 
 	while (*dest)
 		dest++;
 	while (*src)
 		*dest++ = *src++;
-
 	*dest = '\0';
-	return (temp);
+
+	return (point);
 }
