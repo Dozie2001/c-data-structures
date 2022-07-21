62d90dc2bf9ed42d579375074ad8b9e355bc73d3
Dozie2001 <davidchidozie402@gmail.com>
2022-07-21 02:13:10 -0700

    Square root of number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+int sqrt(int num, int sqr);
+/**
+ * _sqrt_recursion - A function that returns the square root
+ * @n: The square  number
+ * Return: square root or  -1 if square root is less than 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt(1, n));
+}
+
+/**
+ * sqrt - Finds the square root
+ * @num: Int value to be returned if squareroot
+ * sqr: The squared value
+ * Return: num or -1
+ */
+
+int sqrt(int num, int sqr)
+{
+	if (num > sqr)
+		return (-1);
+	else if (num * num == sqr)
+		return (num);
+	return (sqrt(num + 1, sqr));
+}
