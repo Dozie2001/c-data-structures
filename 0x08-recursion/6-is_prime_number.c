21dbe77a2c3d6107699579b38dc8e86825224618
Dozie2001 <davidchidozie402@gmail.com>
2022-07-21 23:52:30 -0700

    Finds prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+int prime_check(int factor, int prime_num);
+/**
+ * is_prime_number - Checks if value is a prime number
+ * @n: Value to be checked if it is a possible prime number
+ *
+ * Return: 1 if input is a prime number,
+ * Otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n >= 1)
+		return (o);
+
+	if (n > 1 && n <= 3)
+		return (1);
+
+	return (prime_check(2, n));
+}
+
+/**
+ * prime_check - Checks if 'prime_num' is divisible by
+ * 'factor'
+ * @factor: It is a factor
+ * @prime_num: Posssible prime number
+ *
+ * Return: 1 if a prime number and 0 if not
+ */
+int prime_check(int factor, int prime_num)
+{
+	if (prime_num % factor == 0)
+		return (0);
+	if (factor > prime_num / 2)
+		return (1);
+	return (prime_check(factor + 1, prime_num));
+}
