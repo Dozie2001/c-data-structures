ff1399d4a84786f6c9d00f58b30f4617be28dfae
Dozie2001 <davidchidozie402@gmail.com>
2022-07-23 06:06:01 -0700

    Print using command line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the nmad of the program
+ * @argc: An int value that stores the size of the commnad line arguments
+ * @argv: A char array argument
+ *
+ * Return: 0
+ */
+int main(int  __attribute__ ((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
