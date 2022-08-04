30f54c9abe83edc9b068dcf8e35be17c47255eea
Dozie2001 <davidchidozie402@gmail.com>
2022-08-04 04:40:48 -0700

    bug: undeclared variable corrected

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int = 0;
+	unsigned int i = 0;
 
 	if (action == NULL || array == NULL)
 		return;
 
-	for (; i < sie; i++)
+	for (; i < size; i++)
 		action(array[i]);
 }
