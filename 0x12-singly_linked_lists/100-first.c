31d8fabcc70bac64e306e4493d68892a5797a4c6
Dozie2001 <davidchidozie402@gmail.com>
2022-08-14 06:34:44 -0700

    Bug: A semi colon missing

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,6 +11,6 @@ void main_constructor(void) __attribute__((constructor));
 
 void main_constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n")
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
