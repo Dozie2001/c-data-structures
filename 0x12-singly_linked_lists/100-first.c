32d11c5c2a7d69c66097cc5c666b0f52ce492905
Dozie2001 <davidchidozie402@gmail.com>
2022-08-14 06:25:04 -0700

    Bug:A character longer than needed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,6 +11,6 @@ void main_constructor(void) __attribute__((constructor));
 
 void main_constructor(void)
 {
-	printf("You'e beat! and yet, you must allow,\n");
+	printf("You'e beat! and yet,you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
