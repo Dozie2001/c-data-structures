3a06957468ecd7fbdc20817044b3355f905f03df
Dozie2001 <davidchidozie402@gmail.com>
2022-08-14 06:17:05 -0700

    A function that is executed before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Use Constructor in C */
+void main_constructor(void) __attribut__((constructor));
+
+/**
+ * main_constructor - A constructor function that prints a message
+ * before main runs
+ * Return: Northing
+ */
+
+void main_constructor(void)
+{
+	printf("You'e beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
