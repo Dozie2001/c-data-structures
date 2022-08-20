a1d8ef5e7830243b8cd81043e2a0744d7e779c85
Dozie2001 <davidchidozie402@gmail.com>
2022-08-20 05:27:32 -0700

    Add: A mainfile

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unisgned int base = 1, results = 0, len = 0;
+	unsigned int base = 1, results = 0, len = 0;
 
 	if (b == NULL)
 		return (0);
@@ -21,9 +21,9 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 
 		if (b[len - 1] == '1')
-			result += base;
+			results += base;
 		base *= 2;
 		len--;
 	}
-	return (result);
+	return (results);
 }
