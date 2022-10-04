4f9027f38c9b1d0156687152f1549c092e2461ca
Dozie2001 <davidchidozie402@gmail.com>
2022-10-04 21:02:57 +0100

    Hi

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+      _puts("Hello World!");
+      return (0);
+}
